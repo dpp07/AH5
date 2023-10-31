317e51f1bb151d29458182328c8be70f7c44dd00
Deep Patel <147175602+dpp07@users.noreply.github.com>
2023-10-31 14:53:26 -0400

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,12 +1,29 @@
+import java.util.HashMap;
+
 class Solution {
     public static char mostFrequentChar(String s) {
-      
-      return;
+        HashMap <Character, Integer> charCountMap = new HashMap<>();
+        int storeLength = s.length();
+        char storeAnswer = 0;
+        int count = 0;
+
+        for (int i = 0; i < storeLength; i++){
+            char character = s.charAt(i);
+            charCountMap.put(character, charCountMap.getOrDefault(character, 0) + 1);
+
+            if (count <= charCountMap.get(character)){
+                storeAnswer = character;
+                count = charCountMap.get(character);
+            }
+        }
+      return storeAnswer;
     }
   
     public static void main(String[] args) {
-      // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+      System.out.println(Solution.mostFrequentChar("bookkeeper"));
+      System.out.println(Solution.mostFrequentChar("david"));
+      System.out.println(Solution.mostFrequentChar("mississippi"));
+      System.out.println(Solution.mostFrequentChar("eleventennine"));
     }
 
   }
